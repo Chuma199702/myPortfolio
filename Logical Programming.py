401dbd74a682a8319cb47acea97124f403f8dca9
Chuma199702 <ckohlani@gmail.com>
2026-07-19 15:22:18 +0200

    Add files via upload

---
+++ Logical Programming.py
@@ -0,0 +1,18 @@
+my_name = "Tom"
+if my_name == "Tom":
+ print("I was looking for you")
+num1 = 10
+num2 = 20
+if num1 >= num2:
+ print("It's not possible that 10 is bigger than or equal to 20.")
+else:
+    print("10 is less than 20.")
+team1_score = 3
+team2_score = 2
+game = "Over"
+if (team1_score > team2_score) and (game == "Over"):
+  print("Congratulations Team 1, you have won the match!")
+speed = int(input("How many kilometres per hour are you travelling at?"))
+belt = input("Are you wearing a safety belt?")
+if (speed > 80) or (belt != "Yes"):
+  print("Sorry Sir, but I have to give you a traffic fine.")
