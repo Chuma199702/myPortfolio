3d9b8e4d1c3ed309ab283f3feb6ec1d2a6d47360
Chuma199702 <ckohlani@gmail.com>
2026-07-19 15:21:08 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,24 @@
+# 1. Ask the user to enter their name
+# 2. Store the name in a variable
+# 3. Print the name
+# 4. Ask the user to enter their age
+# 5. Store the age in a variable
+# 6. Print the age
+# 7. Print "Hello, World!" on a new line
+
+# Python implementation
+
+# Ask for user's name
+name = input("Enter your name: ")
+
+# Print the name
+print("Your name is:" + name)
+
+# Ask for user's age
+age = input("Enter your age: ")
+
+# Print the age
+print("Your age is:"+ age)
+
+# Print Hello World message
+print("Hello, World!")
